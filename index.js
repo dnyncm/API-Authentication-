bececa66a8d726e5a1c4c0e0b488b6ac7ce51274
dnyncm <dny.ncm@gmail.com>
2024-11-28 20:27:22 +0700

    Token

--- index.js
+++ index.js
@@ -65,6 +65,24 @@ app.get("/apiKey", async (req, res) => {
     res.render("index.ejs", {error: error.message});
   }
 });
+app.get("/bearerToken", async (req, res) => {
+  //TODO 5:hit up the /secrets/{id} endpoint and get the secret with id of 42
+  //use axios to do bearer token auth
+  try {
+    const response = await axios.get("https://secrets-api.appbrewery.com/secrets/42", {
+      headers: {
+        Authorization: "Bearer e329fcc7-7ff7-4b47-a33a-f61ff96dfd7b",
+      }
+    });
+    const result = JSON.stringify(response.data) ;
+    console.log(result);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {error: error.message});
+  }
+});
+
   
 
 app.listen(port, () => {
